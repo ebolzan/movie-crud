2e6bacdd006d6399e7e7a3324a98ae06068193dc
evandro <ebolzan@inf.ufsm.br>
2021-09-24 00:08:22 -0300

    ready form static

--- src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -11,6 +11,7 @@ import { ValidarCamposService } from 'src/app/shared/components/campos/validar-c
 export class CadastroFilmesComponent implements OnInit {
 
   cadastro: FormGroup;
+  generos: Array<string>;
 
   constructor(public validacao:ValidarCamposService,
     private fb: FormBuilder) { }
@@ -30,6 +31,8 @@ export class CadastroFilmesComponent implements OnInit {
       urlIMDb: ['', [Validators.minLength(10)]],
       genero: ['', [Validators.required]]
     });
+
+    this.generos = ["Ação", "Romance", "Aventura", "Ficção científica", "Comédia"];
 
   }
 
